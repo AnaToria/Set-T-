8f24b16bf4d57f495b88d6bbbe1a7367da0eb896
Ana Toria <72986301+AnaToria@users.noreply.github.com>
2020-12-28 15:42:35 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace COMPE361
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set<int> set1 = new Set<int>();
+            Console.WriteLine("set1 is empty : " + set1.IsEmpty);
+            set1.Add(11);
+            set1.Add(33);
+            set1.Add(22);
+            Console.WriteLine("set1 number of elemnts : " + set1.Count);
+            Console.Write("set1 : ");
+            foreach (var item in set1)
+            {
+                Console.Write(item+ "  ");
+            }
+
+
+            List<int> temp= new List<int>() { 34,0,87,58,100,13};
+            Set<int> set2 = new Set<int>(temp);
+            Console.Write("\nset2 : ");
+            foreach (var item in set2)
+            {
+                Console.Write(item + "  ");
+            }
+
+            Console.Write("\nset1 + set2 : ");
+            Set<int> setProd = set1 + set2;
+            foreach (var item in setProd)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine("\nafter filtring : ");
+            static bool filt(int n)
+            {
+                return (n >= 25) ? true : false;
+            }
+            setProd = setProd.Filter(filt);
+            foreach (var item in setProd)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine("\nremoved 7 : " + setProd.Remove(7));
+            Console.WriteLine("add 123 : " + setProd.Add(123));
+            foreach (var item in setProd)
+            {
+                Console.Write(item + "  ");
+            }
+
+            SortedSet<int> sSet1 = new SortedSet<int>(setProd);
+            Console.WriteLine("\nAfter sorting : ");
+            foreach (var item in sSet1)
+            {
+                Console.Write(item + "  ");
+            }
+
+            List<int> temp2 = new List<int>() { 45, 32, 90, 100, 12, 1 };
+            SortedSet<int> sSet2 = new SortedSet<int>(temp2);
+            Console.WriteLine("\nanother sorted set : ");
+            foreach (var item in sSet1)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.Write("\nadd them up: ");
+            Set<int> sSetProd = sSet1 + sSet2;
+            foreach (var item in sSetProd)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine("\nremoved 32 : " + sSetProd.Remove(32));
+            Console.WriteLine("\nremoved -1 : " + sSetProd.Remove(-1));
+            Console.WriteLine("add 45 : " + sSetProd.Add(45));
+            Console.WriteLine("add 62 : " + sSetProd.Add(62));
+            foreach (var item in sSetProd)
+            {
+                Console.Write(item + "  ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Set.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace COMPE361
+{
+    public class Set<T> : IEnumerable<T>
+    {
+        protected List<T> Accs;
+        public delegate bool F<T>(T elt);
+
+        //properties of Set<T>
+        public int Count
+        {
+            get
+            {
+                return Accs.Count();
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                return !Accs.Any();
+            }
+        }
+        public Set()    //Default constructor
+        {
+            Accs = new List<T>();
+        }
+        public Set(IEnumerable<T> e)    //Parametrized constructor
+        {
+            Accs = new List<T>();
+            foreach (var element in e)   //Initialization
+            {
+                if (!Accs.Contains(element))
+                {
+                    Accs.Add(element);
+                }
+            }
+        }
+        public static Set<T> operator +(Set<T> lhs, Set<T> rhs)    // overloading "+"
+        {
+            Set<T> union = new Set<T>(lhs);
+            foreach (var Accs in rhs)
+            {
+                if (!union.Contains(Accs))
+                {
+                    union.Accs.Add(Accs);
+                }
+            }
+            return union;
+        }
+        public virtual bool Add(T item)
+        {
+            if (!Accs.Contains(item))
+            {
+                Accs.Add(item);
+                return true;
+            }
+            return false;
+        }
+        public bool Remove(T item)
+        {
+            if (Accs.Contains(item))
+            {
+                Accs.Remove(item);
+                return true;
+            }
+
+            return false;
+        }
+        public bool Contains(T item) => Accs.Contains(item); //Checking if item is in set
+        public Set<T> Filter(F<T> filterFunction)
+        {
+            Set<T> filtered = new Set<T>();
+            foreach (var elt in Accs)
+            {
+                if (filterFunction(elt))
+                {
+                    filtered.Accs.Add(elt);
+                }
+            }
+            return filtered;
+        }
+        public IEnumerator<T> GetEnumerator()   //IEnumerable interface
+        {
+            foreach (var elt in Accs)
+            {
+                yield return elt;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace COMPE361
+{
+    // Declaring delegate outside the class
+    public delegate bool F<T>(T elt);
+
+    public class Set<T> : IEnumerable<T>
+    {
+        // Properties
+        protected List<T> Data;
+
+        public int Count
+        {
+            get
+            {
+                return Data.Count();
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return !Data.Any();
+            }
+        }
+
+
+        // Methods
+        // Default constructor
+        public Set()
+        {
+            Data = new List<T>();
+        }
+
+        // Parametrized constructor
+        public Set(IEnumerable<T> e)
+        {
+            Data = new List<T>();
+            // Initializing
+            foreach(var element in e)
+            {
+                // Checking whether element is already in my Set 
+                if (!Data.Contains(element))
+                {
+                    Data.Add(element);
+                }
+            }
+        }
+
+        // Set union by overloading +
+        public static Set<T> operator +(Set<T> lhs, Set<T> rhs)
+        {
+            Set<T> union = new Set<T>(lhs);
+
+            // Adding different elements 
+            foreach (var data in rhs)
+            {
+                if(!union.Contains(data))
+                {
+                    union.Data.Add(data);
+                }
+            }
+
+            return union;
+        }
+
+        // Add item to the set
+        public virtual bool Add(T item)
+        {
+            if (!Data.Contains(item))
+            {
+                Data.Add(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        // Remove item from the set
+        public bool Remove(T item)
+        {
+            if (Data.Contains(item))
+            {
+                Data.Remove(item);
+                return true;
+            }
+
+            return false;
+        }
+
+        // Check if item is in the set
+        public bool Contains(T item) => Data.Contains(item);
+     
+        // Filter
+        public Set<T> Filter(F<T> filterFunction)
+        {
+            Set<T> filteredSet = new Set<T>();
+
+            foreach (var elt in Data)
+            {
+                if (filterFunction(elt))
+                {
+                    filteredSet.Data.Add(elt);
+                }
+            }
+
+            return filteredSet;
+        }
+
+
+        // Implementing IEnumerable interface
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var elt in Data)
+            {
+                yield return elt;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}*/
